7df920302e5baf5fa5dc01b7af56de1f2af38ea1
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-02-08 10:41:25 -0800

    Add functions for movies and reviews

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,5 +3,40 @@ class MoviesController < ApplicationController
   get '/movies' do
     Movie.all.to_json
   end
+
+  get '/movies/:id' do
+    movie = Movie.find_by(id: params[:id])
+    if movie
+      movie.to_json
+    else
+      "404 - Movie not found!"
+    end
+  end
+
+  post '/movies' do
+    Movie.create(
+      name: params[:name],
+      actors: params[:actors],
+      img_link: params[:img_link],
+      genre: params[:genre],
+      year: params[:year],
+      plot: params[:plot],
+      rating: params[:rating]
+    ).to_json
+  end
+
+  patch '/movies/:id' do
+    def updateMovie (option)
+      movie = Movie.find(params[:id])
+      movie.update(option).to_json
+    end
+    updateMovie
+  end
+
+  delete '/movies/:id' do
+    movie = Movie.find(params[:id])
+    movie.destroy
+    movie.to_json
+  end
   
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,5 +3,9 @@ class ReviewsController < ApplicationController
   get '/reviews' do
     Review.all.to_json
   end
+
+  get '/reviews/:movie_id' do
+    Review.find_by(movie_id: params[:movie_id])
+  end
   
 end
