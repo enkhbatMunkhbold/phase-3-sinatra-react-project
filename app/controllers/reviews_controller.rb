87a36fa09740c853cf52fe66782cd74b30ff41bc
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-02-08 21:35:46 -0800

    Current Status: try to modify post reviews on reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,23 @@
 class ReviewsController < ApplicationController
   
-  get '/reviews' do
-    Review.all.to_json
+  get 'movies/:id/reviews' do
+    movie = Movie.find_by(id: params[:id])
+    reviews = movie.reviews.all
+    reviews.all.to_json
   end
 
-  get '/reviews/:movie_id' do
+  get 'movies/:id/reviews/:movie_id' do
     Review.find_by(movie_id: params[:movie_id])
   end
 
-  post '/reviews' do
-    Review.create(
+  post 'movie/:id/reviews' do
+    movie = Movie.find_by(id: params[:id])
+    review = Review.create(
       name: params[:name],
       comments: params[:comments]
     )
+    reviews = movie.reviews << review
+    reviews.to_json
   end
   
 end
