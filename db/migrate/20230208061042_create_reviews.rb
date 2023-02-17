1c9bcd31ef9c03c4bf66df20ebd9e372c1e60ad5
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-02-16 22:43:37 -0800

    Add edit property to review object

--- db/migrate/20230208061042_create_reviews.rb
+++ db/migrate/20230208061042_create_reviews.rb
@@ -4,6 +4,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
       t.string :name
       t.text :comment
       t.integer :movie_id
+      t.boolean :edit
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2023_02_08_061042) do
     t.string "name"
     t.text "comment"
     t.integer "movie_id"
+    t.boolean "edit"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
